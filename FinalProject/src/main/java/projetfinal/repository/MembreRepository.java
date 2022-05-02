8c80173624821ed5955631d20949fa9249b26dfe
LucasAubrun <lucas.aubrun@orange.fr>
2022-05-02 09:46:23 +0200

    Init packages / classes Membre

---
+++ FinalProject/src/main/java/projetfinal/repository/MembreRepository.java
@@ -0,0 +1,9 @@
+package projetfinal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import projetfinal.entities.Membres;
+
+public interface MembreRepository extends CrudRepository<Membres, Long> {
+
+}
