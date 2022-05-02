494b21378788fdb2b41be72908eb4a169ea16e3a
raisonclara <raison.clara@gmail.com>
2022-05-02 10:32:08 +0200

    activites

--- FinalProject/src/main/java/projetfinal/repository/ActivitesRepository.java
+++ FinalProject/src/main/java/projetfinal/repository/ActivitesRepository.java
@@ -1,10 +1,15 @@
 package projetfinal.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import projetfinal.entities.Activites;
 import projetfinal.entities.Membres;
 
 public interface ActivitesRepository extends CrudRepository<Activites, Long>{
+	public List<Activites> findByNom(String nom);
+	public List<Activites> findByNbJoueurs(Integer joueurs);
+	public List<Activites> findByType(String type);
 
 }
