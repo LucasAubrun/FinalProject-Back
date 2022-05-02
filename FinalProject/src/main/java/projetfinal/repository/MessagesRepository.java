25614a273bfc61719eb92675561cc904a7ae1d77
Longevillois <alexandre.roux55@gmail.com>
2022-05-02 10:46:57 +0200

    testf

--- FinalProject/src/main/java/projetfinal/repository/MessagesRepository.java
+++ FinalProject/src/main/java/projetfinal/repository/MessagesRepository.java
@@ -1,5 +1,9 @@
 package projetfinal.repository;
 
-public interface MessagesRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import projetfinal.entities.Messages;
+
+public interface MessagesRepository extends CrudRepository<Messages, Long>{
 
 }
