86fd1ac671084f1d7ea7d98407c91d51c6259cad
MatocheB <matthieu.beauchet@gmail.com>
2022-05-03 15:05:16 +0200

    rien

--- FinalProject/src/main/java/projetfinal/repository/MessagesRepository.java
+++ FinalProject/src/main/java/projetfinal/repository/MessagesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import projetfinal.entities.Messages;
 
 public interface MessagesRepository extends CrudRepository<Messages, Long>{
-//jdfbje
+
 }
