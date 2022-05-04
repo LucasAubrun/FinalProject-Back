addf5e55550f81fdf24403788103d144b5cac332
raisonclara <raison.clara@gmail.com>
2022-05-04 11:20:26 +0200

    kouloukoukou

--- FinalProject/src/main/java/projetfinal/entities/Activites.java
+++ FinalProject/src/main/java/projetfinal/entities/Activites.java
@@ -1,7 +1,9 @@
 package projetfinal.entities;
 
+import java.sql.Clob;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
