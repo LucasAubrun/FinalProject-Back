9b106318f67b1cbed460701c8cf31ad1bd007924
raisonclara <raison.clara@gmail.com>
2022-05-06 09:17:28 +0200

    jj

--- FinalProject/src/main/java/projetfinal/repository/MembresRepository.java
+++ FinalProject/src/main/java/projetfinal/repository/MembresRepository.java
@@ -3,13 +3,17 @@ package projetfinal.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import projetfinal.entities.Activites;
 import projetfinal.entities.Membres;
 
 public interface MembresRepository extends CrudRepository<Membres, Long> {
 	
 	public Optional<Membres> findById(Long id);
 	public Optional<Membres> findByMailAndMdp(String mail, String mdp);
+	@Query("SELECT m.photoprofil FROM Membres m WHERE m.id =?1")
+	public Optional<Membres> getPhotoprofilById(Long id);
 
 }
